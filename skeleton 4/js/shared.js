72a42065468dd3aa776bdff9aab03bb2933d691a
tywphgod <87664688+tywphgod@users.noreply.github.com>
2021-07-20 02:53:45 +1000

    Add files via upload

---
+++ skeleton 4/js/shared.js
@@ -0,0 +1,11 @@
+// Shared code needed by the code of all three pages.
+
+// Prefix to use for Local Storage.  You may change this.
+var APP_PREFIX = "monash.eng1003.fencingApp";
+
+// Array of saved Region objects.
+var savedRegions = [];
+
+
+
+
